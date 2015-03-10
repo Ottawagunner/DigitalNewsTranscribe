ef9d13e30d3628d94718944d8496c7330c20d44b
Matthew Taylor <matthewtaylor3@cmail.carleton.ca>
2015-03-10 15:03:40 -0400

    Update CommentScraperReddit.py

--- CommentScraperReddit.py
+++ CommentScraperReddit.py
@@ -1,11 +1,17 @@
 import praw
 import time
 import datetime
-
+import sys
 r = praw.Reddit('all_comment_scraper 1.0 by u/ottawagunner')
+r.login('ottabot', 'testbot')
 #flat_comments = praw.helpers.flatten_tree(all_comments)
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 print "Welcome to Reddit Comment Data Miner"
 print "-Built with PRAW"
+print(sys.version)
+
 #subreddit = r.get_subreddit('learnpython')
 done = []
 KeyWords = []
@@ -23,17 +29,13 @@ with open('searchList.txt') as search_file:
 KeyWords.extend(listWords)
 KeyWords.extend(map(lambda x:x.title(),listWords))
 
-
 print "Looking for :"
 print '\n'.join(KeyWords)
 
 while True:
 	print "Grabbing Comments %s" % datetime.datetime.time(datetime.datetime.now())
-	subreddit = r.get_subreddit(subname)
-	all_comments = subreddit.get_comments(limit=None)
-	#all_comments = r.get_comments('all')
-	for comment in all_comments:
-		has_keywords = any(string in comment.body.encode("utf-8") for string in KeyWords)
+	for comment in praw.helpers.comment_stream(r, subname, limit=None):
+		has_keywords = any(string in comment.body for string in KeyWords)
 		if comment.id not in done and has_keywords:
 			print "Downloading comment by %s" % comment.author.name
 			with open(filename, 'a+') as the_file:
@@ -42,8 +44,5 @@ while True:
 				the_file.write("%s;" % datetime.datetime.fromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S').encode('utf-8'))
 				the_file.write("%s;\n" % comment.body.replace('\n', ' ').replace('\r', '').replace(';',' ').encode('utf-8'))
 				done.append(comment.id)
-	print "Waiting 30 seconds"
-	if len(done) >= 7000:
-		done[:] = []
-		print "More than 7000 elements, clearing... "
-	time.sleep(30)
+	print "Waiting 45 seconds"
+	time.sleep(45)
